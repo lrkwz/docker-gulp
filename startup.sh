2de156e408c0f051364d3b6106439ad6cfb94c19
Luca Orlandi <luca.orlandi@gmail.com>
2017-02-13 22:20:28 +0100

    Fixed dependency install

--- startup.sh
+++ startup.sh
@@ -3,7 +3,8 @@
 #npm install gulp
 #npm install gulp-sass
 #npm install --save-dev gulp-install
-npm install gulp -D
+npm install
+#npm install gulp --save-dev
 if [ ! -f ./gulpfile.js ]; then
     touch gulpfile.js
 fi
